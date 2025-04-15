4f2090f3eb78676634082f0d5800eb06b12c5158
shiflux <harshtamrakar474@gmail.com>
2025-04-15 23:45:27 +0530

    4

--- src/gqlClient.ts
+++ src/gqlClient.ts
@@ -1,6 +1,8 @@
 import { GraphQLClient } from 'graphql-request';
 import { RequestDocument } from 'graphql-request';
-const graphqlClient = new GraphQLClient('http://localhost:8000/graphql'); // replace with your server URL
+
+const url = import.meta.env.VITE_URL; // replace with your server URL
+const graphqlClient = new GraphQLClient(`${url}/graphql`); // replace with your server URL
 
 export const fetcher = <TData>(query: RequestDocument, variables?: any): Promise<TData> => {
     return graphqlClient.request(query, variables);
